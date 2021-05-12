60cdded91a3c4e7236f60a562ae8ff2eede4f7f8
Leyla Yılmaz <leylay1996@gmail.com>
2021-05-12 20:57:11 +0300

    change template

---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,10 @@
+<script src="{{ asset('js/jquery-1.11.3.min.js') }}"></script> 
+<script src="{{ asset('js/bootstrap.min.js') }}"></script> 
+<script src="{{ asset('js/own-menu.js') }}"></script> 
+<script src="{{ asset('js/jquery.lighter.js') }}"></script> 
+<script src="{{ asset('js/owl.carousel.min.js') }}"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="{{ asset('rs-plugin/js/jquery.tp.t.min.js') }}"></script> 
+<script type="text/javascript" src="{{ asset('rs-plugin/js/jquery.tp.min.js') }}"></script> 
+<script src="{{ asset('js/main.js') }}"></script> 
